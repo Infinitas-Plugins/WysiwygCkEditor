174b4d41a7cc3a6dfbbe91a79cca6cddce34ed5e
dogmatic69 <dogmatic69@gmail.com>
2010-11-11 17:16:37 +0000

    creating a release for the ck editor plugin

---
+++ config/releases/map.php
@@ -0,0 +1,6 @@
+<?php
+$map = array(
+	1 => array(
+		'000001_wysiwyg_ck_editor' => 'R4cdc231840484988b640247a6318cd70'),
+);
+?>
---
+++ config/schema/schema.php
@@ -0,0 +1,15 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* WysiwygCkEditor schema generated on: 2010-11-11 17:11:40 : 1289495320*/
+class WysiwygCkEditorSchema extends CakeSchema {
+	var $name = 'WysiwygCkEditor';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+}
+?>
