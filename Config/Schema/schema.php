6e7746600ff98819a41e6055c94991f7a68d240a
dogmatic69 <dogmatic69@gmail.com>
2012-03-06 17:35:25 +0000

    updating the ck editor release

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -1,15 +1,11 @@
-<?php 
-/* SVN FILE: $Id$ */
-/* WysiwygCkEditor schema generated on: 2010-11-11 17:11:40 : 1289495320*/
+<?php
 class WysiwygCkEditorSchema extends CakeSchema {
-	var $name = 'WysiwygCkEditor';
 
-	function before($event = array()) {
+	public function before($event = array()) {
 		return true;
 	}
 
-	function after($event = array()) {
+	public function after($event = array()) {
 	}
 
 }
-?>
--- Config/releases/map.php
+++ Config/releases/map.php
@@ -1,6 +1,6 @@
 <?php
 $map = array(
 	1 => array(
-		'000001_wysiwyg_ck_editor' => 'R4cdc231840484988b640247a6318cd70'),
+		'000009_WysiwygCkEditor' => 'R4f564ab974e0413180e54d916318cd70'),
 );
 ?>
