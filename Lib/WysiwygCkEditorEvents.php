4854374eccbdf7824c3583ca0ab49765432cf082
dogmatic69 <dogmatic69@gmail.com>
2013-01-19 01:15:02 +0000

    update to the latest version of ck editor

--- Lib/WysiwygCkEditorEvents.php
+++ Lib/WysiwygCkEditorEvents.php
@@ -1,38 +1,39 @@
 <?php
-	/**
-	 * events for ck_editor
-	 */
-	class WysiwygCkEditorEvents extends AppEvents {
-		function onRegisterWysiwyg(Event $Event) {
-			return 'ck_editor';
+/**
+ * WysiwygCkEditorEvents
+ */
+class WysiwygCkEditorEvents extends AppEvents {
+
+	public function onRegisterWysiwyg(Event $Event) {
+		return 'ck_editor';
+	}
+
+	public function onRequireJavascriptToLoad(Event $Event) {
+		if(Configure::read('Wysiwyg.editor') != 'ck_editor') {
+			return false;
 		}
 
-		function onRequireJavascriptToLoad(Event $Event) {
-			if(Configure::read('Wysiwyg.editor') != 'ck_editor') {
-				return false;
+		if(isset($Event->Handler->request->params['admin']) && $Event->Handler->request->params['admin']) {
+			if(in_array($Event->Handler->request->params['action'], array('admin_edit', 'admin_add'))) {
+				return array(
+					'WysiwygCkEditor.ckeditor'
+				);
 			}
-
-			if(isset($Event->Handler->request->params['admin']) && $Event->Handler->request->params['admin']) {
-				if(in_array($Event->Handler->request->params['action'], array('admin_edit', 'admin_add'))) {
-					return array(
-						'WysiwygCkEditor.ckeditor'
-					);
-				}
-			}
-		}
-
-		public function onSetupRoutes(Event $Event) {
-			InfinitasRouter::connect(
-				'/wysiwyg_ck_editor/js/contents.get.css',
-				array(
-					'plugin' => 'themes',
-					'controller' => 'themes',
-					'action' => 'frontend_css',
-					'admin' => false
-				),
-				array(
-					'ext' => 'css'
-				)
-			);
 		}
 	}
+
+	public function onSetupRoutes(Event $Event) {
+		InfinitasRouter::connect(
+			'/wysiwyg_ck_editor/js/contents.get.css',
+			array(
+				'plugin' => 'themes',
+				'controller' => 'themes',
+				'action' => 'frontend_css',
+				'admin' => false
+			),
+			array(
+				'ext' => 'css'
+			)
+		);
+	}
+}
--- View/Helper/WysiwygCkEditorHelper.php
+++ View/Helper/WysiwygCkEditorHelper.php
@@ -1,73 +1,68 @@
 <?php
-	/**
-	 * TinyMce Wysiwig Helper
-	 *
-	 * This is the helper to load and cofigure the tiny mce editor for infinitas
-	 *
-	 * Copyright (c) 2010 Carl Sutton ( dogmatic69 )
-	 *
-	 * @filesource
-	 * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
-	 * @link http://www.infinitas-cms.org
-	 * @package wysiwyg
-	 * @subpackage wysiwyg.helpers.ck_editor
-	 * @license http://www.opensource.org/licenses/mit-license.php The MIT License
-	 * @since 0.6a
-	 *
-	 * @author Carl Sutton ( dogmatic69 )
-	 *
-	 * Licensed under The MIT License
-	 * Redistributions of files must retain the above copyright notice.
-	 */
+/**
+ * TinyMce Wysiwig Helper
+ *
+ * This is the helper to load and cofigure the tiny mce editor for infinitas
+ *
+ * @copyright Copyright (c) 2010 Carl Sutton ( dogmatic69 )
+ *
+ * @link http://www.infinitas-cms.org
+ * @package wysiwyg
+ * @subpackage wysiwyg.helpers.ck_editor
+ * @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @since 0.6a
+ *
+ * @author Carl Sutton <dogmatic69@infinitas-cms.org>
+ */
 
-	App::uses('InfinitasHelper', 'Libs.View/Helper');
-	class WysiwygCkEditorHelper extends AppHelper {
-		/**
-		 * @var array
-		 * @access protected
-		 */
-		public $_defaults = array();
+App::uses('InfinitasHelper', 'Libs.View/Helper');
 
-		public $helpers = array(
-			'Html'
-		);
+class WysiwygCkEditorHelper extends AppHelper {
 
-		/**
-		 * Load the editor.
-		 *
-		 * This is the method that will replace the html element with the editor
-		 *
-		 * @param sring $fieldName the name of the field to replace.
-		 *
-		 * @return string the javascript code to load the editor.
-		 */
-		public function editor($fieldName = null, $config = array()) {
-			$did = $lines = '';
+/**
+ * Helpers to load
+ *
+ * @var array
+ */
+	public $helpers = array(
+		'Html'
+	);
 
-			foreach (explode('.', $fieldName) as $v) {
-				$did .= ucfirst(Inflector::camelize($v));
-			}
+/**
+ * Load the editor.
+ *
+ * This is the method that will replace the html element with the editor
+ *
+ * @param sring $fieldName the name of the field to replace.
+ * @param array $config the config options
+ *
+ * @return string
+ */
+	public function editor($fieldName = null, $config = array()) {
+		$did = $lines = '';
 
-			$options = array_merge($this->_defaults, $config);
-
-			$lines = array();
-
-			foreach ($options as $option => $value) {
-				$lines[] = $option . ' : "' . $value . '"';
-			}
-			$lines[] = 'customConfig:"/wysiwyg_ck_editor/js/config.js"';
-
-			$lines = implode(', ', $lines);
-			return $this->Html->scriptBlock(
-				sprintf(
-					"CKEDITOR.replace( '%s', { %s });",
-					$did,
-					$lines
-				),
-				array(
-					'inline' => false,
-					'block' => 'scripts_for_layout'
-				)
-			);
+		foreach (explode('.', $fieldName) as $v) {
+			$did .= ucfirst(Inflector::camelize($v));
 		}
+
+		$lines = array();
+
+		foreach ($config as $option => $value) {
+			$lines[] = $option . ' : "' . $value . '"';
+		}
+		$lines[] = 'customConfig:"/wysiwyg_ck_editor/js/config.js"';
+
+		$lines = implode(', ', $lines);
+		return $this->Html->scriptBlock(
+			sprintf(
+				"CKEDITOR.replace( '%s', { %s });",
+				$did,
+				$lines
+			),
+			array(
+				'inline' => false,
+				'block' => 'scripts_for_layout'
+			)
+		);
 	}
+}
