293c4eb4026b5ff6ffa0673db01661cbd54aa3c3
dogmatic69 <dogmatic69@gmail.com>
2013-01-25 17:07:16 +0000

    adding tag linking

--- webroot/js/plugins/infinitas/dialogs/infinitasRelated.js
+++ webroot/js/plugins/infinitas/dialogs/infinitasRelated.js
@@ -3,15 +3,24 @@
  */
 
 CKEDITOR.dialog.add('infinitasRelatedDialog', function(editor) {
-	var content = jQuery('ul.related-content li'),
-		elements = [];
-	jQuery.each(content, function(k, v) {
-		v = jQuery(v);
-		debug(v);
-		elements.push([
-			v.data('text'), v.data('url')
-		]);
-	});
+	var records = jQuery('ul.related-content.records li'),
+		tags = jQuery('ul.related-content.tags li'),
+		elements = {
+			records: [],
+			tags: []
+		};
+		jQuery.each(records, function(k, v) {
+			v = jQuery(v);
+			elements.records.push([
+				v.data('text'), v.data('url')
+			]);
+		});
+		jQuery.each(tags, function(k, v) {
+			v = jQuery(v);
+			elements.tags.push([
+				v.data('text'), v.data('url')
+			]);
+		});
 	return {
 		title: 'Related content',
 		minWidth: 600,
@@ -20,14 +29,18 @@ CKEDITOR.dialog.add('infinitasRelatedDialog', function(editor) {
 			id: 'infinitas-related-tab',
 			elements: [{
 				type: 'select',
-				id: 'infinitas-related-url',
-				label: 'Record to link to',
-				items: elements,
-				validate: CKEDITOR.dialog.validate.notEmpty('Select the content to link to')
+				id: 'infinitas-related-record-url',
+				label: 'Link to a record',
+				items: elements.records
+			}, {
+				type: 'select',
+				id: 'infinitas-related-tag-url',
+				label: 'Link to a tag',
+				items: elements.tags
 			}, {
 				type: 'text',
 				id: 'infinitas-related-text',
-				label: 'Link text',
+				label: 'Link text'
 			}]
 		}],
 
@@ -36,10 +49,15 @@ CKEDITOR.dialog.add('infinitasRelatedDialog', function(editor) {
 				link = editor.document.createElement('a'),
 				text = dialog.getValueOf('infinitas-related-tab', 'infinitas-related-text');
 
-			if (text == '') {
+			if (!text) {
 				text = editor.getSelection().getSelectedText();
 			}
-			link.setAttribute('href', dialog.getValueOf('infinitas-related-tab', 'infinitas-related-url'));
+			url = dialog.getValueOf('infinitas-related-tab', 'infinitas-related-record-url');
+			if (!url) {
+				url = dialog.getValueOf('infinitas-related-tab', 'infinitas-related-tag-url');
+			}
+			
+			link.setAttribute('href', url);
 			link.setAttribute('title', text);
 			link.setHtml(text);
 
